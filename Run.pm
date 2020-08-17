ca5eb7f4db22e4972e5ec18640290929210892f0
szTheory <szTheory@users.noreply.github.com>
2020-08-17 18:38:38 -0400

    read JSON newsletters list

---
+++ Run.pm
@@ -0,0 +1,13 @@
+package Run 1.0;
+
+use strict;
+use warnings;
+
+# use FindBin;
+# use lib "$FindBin::RealScript";
+
+use lib 'lib';    #tell perl we'll find modules in lib/
+
+use Build 'newsletters';
+
+1;
---
+++ cpanfile
@@ -0,0 +1,3 @@
+requires 'perl', '5.32.0';
+
+requires 'JSON::MaybeXS', '1.004002';
